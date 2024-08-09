f2cfdc10cd9e2b8ff30a98e523a9f0dc83ea0d96
1mmanuel <xeris19036@klefv.com>
2024-08-09 11:05:57 +0300

    Implemented AddTransaction

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,14 +1,9 @@
 import React, { Children, createContext, useReducer } from "react";
 import AppReducer from "./AppReducer";
-import { type } from "@testing-library/user-event/dist/type";
+
 //Initial state
 const initialState = {
-  transactions: [
-    { id: 1, text: "Flower", amount: -20 },
-    { id: 2, text: "Salary", amount: 300 },
-    { id: 3, text: "Book", amount: -10 },
-    { id: 4, text: "Camera", amount: 150 },
-  ],
+  transactions: [],
 };
 
 // Create context
@@ -26,9 +21,20 @@ export const GlobalProvider = ({ children }) => {
     });
   }
 
+  function addTransaction(transaction) {
+    dispatch({
+      type: "ADD_TRANSACTION",
+      payload: transaction,
+    });
+  }
+
   return (
     <GlobalContext.Provider
-      value={{ transactions: state.transactions, deleteTransaction }}
+      value={{
+        transactions: state.transactions,
+        deleteTransaction,
+        addTransaction,
+      }}
     >
       {children}
     </GlobalContext.Provider>
